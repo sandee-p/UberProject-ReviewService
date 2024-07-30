a80af5720305cb08454f6ce5cab67a3d5a5eee9f
Sandeep <sandeep9899kumar@outlook.com>
2024-07-30 21:00:40 +0530

    Added more columns in the booking review table

---
+++ src/main/java/org/example/uberreviewservice/models/Review.java
@@ -0,0 +1,38 @@
+package org.example.uberreviewservice.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity // will work on the java logic layer
+@Table(name = "bookingreview") // will only work in DB level
+public class Review {
+
+    @Id // annotation to make the id property -> primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // id property will not be feeded by the user, it needs to be created.
+    private Long id;
+
+    @Column(nullable = false)
+    private String reviewContent;
+
+    private Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP) // this annotation tells spring about the format of Date object we are going to store
+    @CreatedDate // this annotation tells spring that only handle it for object creation
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate // this annotation tells spring that only handle it for object update
+    private Date updatedAt;
+
+}
---
+++ src/main/java/org/example/uberreviewservice/repositories/ReviewRepository.java
@@ -0,0 +1,7 @@
+package org.example.uberreviewservice.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReviewRepository {
+}
