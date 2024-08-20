5b94360da43061fdf615a7281445207118d4d2bd
Sandeep <sandeep9899kumar@outlook.com>
2024-08-20 21:00:53 +0530

    Added base model class using mapped superclass

---
+++ src/main/java/org/example/uberreviewservice/models/BaseModel.java
@@ -0,0 +1,34 @@
+package org.example.uberreviewservice.models;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass // helps handle inheritance in spring data jpa
+@Getter
+@Setter
+public abstract class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP) // this annotation tells spring about the format of Date object we are going to store
+    @CreatedDate // this annotation tells spring that only handle it for object creation
+    protected Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate // this annotation tells spring that only handle it for object update
+    protected Date updatedAt;
+
+}
--- src/main/java/org/example/uberreviewservice/models/Review.java
+++ src/main/java/org/example/uberreviewservice/models/Review.java
@@ -1,40 +1,23 @@
 package org.example.uberreviewservice.models;
 
+import com.fasterxml.jackson.databind.ser.Serializers;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.util.Date;
 
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
+
 @Entity // will work on the java logic layer
 @Table(name = "bookingreview") // will only work in DB level
-public class Review {
-
-    @Id // annotation to make the id property -> primary key
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // id property will not be feeded by the user, it needs to be created.
-    private Long id;
+public class Review extends BaseModel {
 
     @Column(nullable = false)
     private String reviewContent;
 
     private Double rating;
-
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP) // this annotation tells spring about the format of Date object we are going to store
-    @CreatedDate // this annotation tells spring that only handle it for object creation
-    private Date createdAt;
-
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate // this annotation tells spring that only handle it for object update
-    private Date updatedAt;
 
 }
